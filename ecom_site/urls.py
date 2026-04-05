0398223375d04972046aee2223a93a50c6449c9b
Anupriya Jaisheela <anupriyajaisheela@gmail.com>
2026-04-05 10:17:13 +0000

    Final homepage and media fix

--- ecom_site/urls.py
+++ ecom_site/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from  django.conf import settings
+from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# This ensures images and CSS work on Render
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
